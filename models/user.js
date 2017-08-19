9afdce2d72e3e83e941d039937901d824b2c8416
moeabdol <mohd.a.saed@gmail.com>
2017-08-19 22:26:33 +0300

    Create user model

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const bcrypt   = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["Client", "Manager", "Admin"],
+    default: "Client"
+  }
+});
+
+UserSchema.pre("save", function(next) {
+  const user = this;
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) { return next(err); }
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) { return next(err); }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+UserSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) { return done(err); }
+    done(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
